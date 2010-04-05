9acbe9f3858098fdb5c1104a4d1bb8bde2695253
maldonado.celeste <maldonado.celeste@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-05 04:06:59 +0000

    Solo mejore la doc doxygen.

--- application/entities/KeyInt.h
+++ application/entities/KeyInt.h
@@ -1,8 +1,6 @@
-/*
- * KeyInt.h
+/**
+ * @file KeyInt.h
  *
- *  Created on: 02/04/2010
- *      Author: kira
  */
 
 #ifndef KEYINT_H_
@@ -26,10 +24,13 @@ private:
 public:
 	KeyInt();
 
+	/// Constructor. Recibe un entero.
 	KeyInt(int value);
 
+	/// Constructor de copia.
 	KeyInt(const KeyInt& key);
 
+	/// Destructor.
 	~KeyInt();
 
 	/**
@@ -39,23 +40,58 @@ public:
 	unsigned int getSize() const;
 
 	/**
-	 * Serializa el registro en bytes
-	 * \param bytes Cadena de bytes en la que almacena el contenido del registro
-	 * \return El puntero a la cadena de bytes
+	 * Convierte el registro a una cadena de bytes.
+	 * Convierte cada atributo mediante los metodos definidos en
+	 * ByteConverter
+	 * @param bytes cadena en la cual se serializan los atributos
+	 * @return cadena resultante al serializar
+	 * @see ByteConverter
 	 */
 	char* serialize(char* bytes) const;
+
 	/**
 	 * Transforma la cadena de bytes a un registro
-	 * \param bytes Cadena de bytes de la cual lee para setear los campos del registro.
+	 * @param bytes Cadena de bytes de la cual lee para setear los campos del registro.
 	 */
 	void deserialize(const char* bytes);
 
+	//-----------Operators------------------------//
+	/**
+	 * Operador de asignacion.
+	 * @param registro Registro de la misma clase contra el cual se hace la asignacion.
+	 * @return referencia a Register
+	 */
 	Register& operator=(const Register& registro);
 
+	/**
+	 * Operador de comparacion de igualdad.
+	 * Tiene sentido su definición para aquellos registros que tengan clave, como es el caso de
+	 * KeyInt.
+	 * El metodo no esta definido virtual puro. Para las clases que no lo implementan este
+	 * metodo tira una excepcion.
+	 * @param registro Registro de la misma clase contra el cual se hace la comparacion.
+	 * @return TRUE si es igual, FALSE de lo contrario.
+	 */
 	bool operator==(const Register& registro)const;
 
+	/**
+	 * Operador de comparacion por menor.
+	 * Tiene sentido su definición para aquellos registros que tengan clave.
+	 * El metodo no esta definido virtual puro. Para las clases que no lo implementan este
+	 * metodo tira una excepcion.
+	 * @param registro Registro de la misma clase contra el cual se hace la comparacion.
+	 * @return TRUE si es menor, FALSE de lo contrario.
+	 */
 	bool operator<(const Register& registro)const;
 
+	/**
+	 * Operador de comparacion por mayor.
+	 * Tiene sentido su definición para aquellos registros que tengan clave.
+	 * El metodo no esta definido virtual puro. Para las clases que no lo implementan este
+	 * metodo tira una excepcion.
+	 * @param registro Registro de la misma clase contra el cual se hace la comparacion.
+	 * @return TRUE si es mayor, FALSE de lo contrario.
+	 */
 	bool operator>(const Register& registro)const;
 
 	/**
@@ -64,7 +100,13 @@ public:
 	 */
 	void setFields(const Register& registro);
 
-
+	/**
+	 * Devuelve un string con el contenido del registro.
+	 * Se debe definir en cada clase obligatoriamente, para
+	 * ser utilizada por el operador << en Register
+	 * @return string con la informacion contenida en el registro
+	 * @see Register operator <<
+	 */
 	std::string toString() const;
 
 	/**
