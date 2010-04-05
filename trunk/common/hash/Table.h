0bd6971711016cc0dbf12b9e29d5bcf13ea353d4
pablolisio <pablolisio@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-05 01:40:26 +0000

    agrego clase que se encarga de manejar la tabla para el hash

---
+++ trunk/common/hash/Table.h
@@ -0,0 +1,96 @@
+/*
+ * Table.h
+ *
+ *  Created on: 04/04/2010
+ *      Author: pablo
+ */
+
+#ifndef TABLE_H_
+#define TABLE_H_
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+class Table {
+public:
+	Table();
+	virtual ~Table();
+
+	/**
+	 * Crea el archivo de la tabla (tabla.txt) y lo inicializa con tamTabla = 0
+	 * y primer registro = 0 (apuntando al bloque 0 del archivo de Hash)
+	 * NOTA: tabla.txt va a tener como primer registro el tamaño de si misma
+	 */
+	FILE * crearArchTabla();
+
+	/**
+	 * Se encarga de abrir la tabla, devolver un listado de los elementos de la tabla
+	 * mediante el puntero listaElementosTabla y luego cerrar el archivo Tabla.
+	 * La funcion devuelve el tamaño de la tabla.
+	 * Nota: recordar que el primer elemento de la tabla es su propio tamaño (tamTabla)
+	 */
+	int parsearTabla(int * listaElementosTabla);
+
+	/**
+	 * Retorna la cant de elementos que tiene la tabla segun el primer renglon
+	 * el cual contiene el tamaño de la misma
+	 */
+	int obtenerTamanioTabla();
+
+	/**
+	 * Retorna el bloque a apuntar en el archivo de Hash segun la posicion en la tabla
+	 * pasada como parametro(registroAApuntarEnTabla)
+	 * Retorna -1 si hubo algun error al intentar leer el registro
+	 */
+	int obtenerBloqueAApuntarEnArchivoHash(int registroAApuntarEnTabla);
+
+	/**
+	 * Modifica el registro "numReg" en la tabla con el valorNuevo a cambiar
+	 */
+	void modificarRegistroEnTabla(int numReg,int valorNuevo);
+
+	/**
+	 * Se le pasa una lista de elementos y el tamaño de la tabla para imprimir la tabla por consola
+	 */
+	void imprimirTabla(int * listaElementosTabla,int tamTabla);
+
+	/**
+	 * Duplica la tabla con los mismos contenidos que posee
+	 */
+	void duplicarTabla();
+
+private:
+	/**
+	 * Se encarga de abrir el archivo de la Tabla segun el formato (lectura,escritura) que se
+	 * le pase como parametro.
+	 * informar si hubo error al intentar hacerlo
+	 */
+	FILE * abrirArchTabla(char formato[2]);
+
+	/**
+	 * Se encarga de abrir el archivo de la Tabla para lectura
+	 */
+	FILE * abrirArchTablaLectura();
+
+	/**
+	 * Se encarga de abrir el archivo de la Tabla para escritura
+	 */
+	FILE * abrirArchTablaEscritura();
+
+	/**
+	 * Se encarga de cerrar el archivo de la Tabla e informar si hubo error al intentar hacerlo
+	 */
+	void cerrarArchTabla(FILE * arch_tabla);
+
+	/**
+	 * Crea archivo temporario para uso de modificacion de datos
+	 */
+	FILE* crearArchivoTemporal();
+
+
+};
+
+#endif /* TABLE_H_ */
