cf01064b95d064449b1eb5bdbbdf6deae375a680
alexmantaut <alexmantaut@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-23 03:04:05 +0000

    se manda archivo que cambio

---
+++ trunk/physical/file/File.h
@@ -0,0 +1,68 @@
+/*
+ * File.h
+ *
+ *  Created on: 22/04/2010
+ *      Author: alex
+ */
+#include <fstream>
+
+#ifndef FILE_H_
+#define FILE_H_
+
+
+/**
+ * Clase abstracta de la cual derivan los archivos
+ * Provee una interfaz basica para los distintos tipos de archivos
+ */
+class File
+{
+public:
+	File();
+	virtual ~File();
+
+	/**
+	 * Abre el archivo. Si no existe se crea.
+	 * @param fileName nombre del archivo.
+	 * @return bool true en caso de exito, false en caso contrario
+	 */
+	virtual bool open(const std::string fileName)=0;
+
+	/**
+	 * Cierra el archivo, libera los FD, etc
+	 * @return bool true en caso de exito, false en caso contrario
+	 */
+	virtual bool close()=0;
+
+protected:
+	//-----------------------------Headers-----------------------------//
+	/**
+	 * Escribe la informacion de control del archivo en disco
+	 * @return bool true en caso de exito, false en caso contrario
+	 */
+	virtual bool writeHeader()=0;
+
+	/**
+	 * Lee la informacion de control del archivo de disco
+	 * @return bool true en caso de exito, false en caso contrario
+	 */
+	virtual bool readHeader()=0;
+
+protected:
+	/**
+	 * FileHandler del archivo, se mantiene a lo largo de toda la vida de
+	 * la clase BlockFile
+	 */
+	std::fstream m_FileHandler;
+
+	/**
+	 * Nombre del archivo
+	 */
+	std::string m_FileName;
+
+	/**
+	 * Tamaño fisico del archivo
+	 */
+	unsigned int m_FileSize;
+};
+
+#endif /* FILE_H_ */
