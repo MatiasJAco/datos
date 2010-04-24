c34141e9e9838587e063374685940a0ed512b531
alexmantaut <alexmantaut@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-24 02:36:32 +0000

    Eliminado contenido anterior del testo...queda implementar..

--- trunk/testNode/testNode.cpp
+++ trunk/testNode/testNode.cpp
@@ -5,55 +5,10 @@
  *      Author: kira
  */
 
-#include "../common/tree/LeafNode.cpp"
-#include "../application/entities/StringRegister.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	LeafNode *lf = new  LeafNode(0,512,50);
-
-//	if(lf->getUsedSpace()!=0)
-//		cout << "Error en GetUsedSpace 1"<<endl;
-
-	StringRegister s(1,"Hola");
-	StringRegister s2(2,"Mundo");
-	StringRegister s3(3,"S");
-	StringRegister s4(2,"af");
-	StringRegister s5(5,"fadsfads");
-
-	lf->insert(s);
-	lf->insert(s2);
-	lf->insert(s3);
-	lf->insert(s4);
-	lf->insert(s5);
-
-//	if( lf->getUsedSpace()!=9)
-//		cout << "Error en GetUsedSpace 2"<<endl;
-
-	cout << *lf << endl;
-
-	KeyInt k(3);
-	lf->remove(k);
-
-	cout << *lf<< endl;
-
-	KeyInt k2(1);
-	StringRegister sk;
-	lf->find(k2,sk);
-
-	cout << sk<< endl;
-
-	KeyInt k3(2);
-	StringRegister sm(6,"Celeste");
-	lf->modify(k3,sm);
-
-	cout << *lf<< endl;
-
-
-
-
-	delete lf;
 	return 0;
 }
