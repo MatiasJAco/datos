374b0f01acf7deaa76e5d8720d32590077f54acb
pablolisio <pablolisio@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-25 01:22:37 +0000

    Empece a modificar la implementacion del add para Hash modif varias irrelevantes en Table y modifique el test de hash

--- trunk/logic/hash/Hash.h
+++ trunk/logic/hash/Hash.h
@@ -21,6 +21,29 @@ private:
 	BlockFile* hashFile;
 	list<Bucket> buckets;
 
+
+	/**
+	 * Retorna el resultado de la funcion Hash. En este caso estamos usando el metodo de "bits sufijos"
+	 */
+	int calculateHashFunction(StringInputData* sid);
+
+	/**
+	 * Retorna el numero de Bucket del HashFile
+	 */
+	int getNumberOfBucket(StringInputData* sid);
+
+	/**
+	 * Crea un bucket (bucket numero 0). Este bucket solo tiene como campo de control el
+	 * tamaño de dispersion inicial (1).
+	 * Es decir que va a insertarle un primer registro variable que indica el tam de disp del bucket
+	 */
+	void inicializeHashFile();
+
+	/**
+	 * Retorna true si ya existe en el HashFile el elemento (sid) pasado por parametro
+	 */
+	bool existsElement(StringInputData* sid);
+
 public:
 
 	Hash();
@@ -28,14 +51,22 @@ public:
 	virtual ~Hash();
 
 	/**
-	 * This method adds a value to the hash.
+	 * Agrega un elemento nuevo al archivo Hash.
+	 * Si el StringInputData ya existe en el HashFile retorna 1;
+	 * Si el StringInputData fue guardado exitosamente retorna 0;
+	 * Si hubo algun problema al ejecutar este metodo retorna -1;
 	 */
-	void add(StringInputData* sid);
+	int add(StringInputData* sid);
 
 	/**
 	 * This method gets a Data object, giving it's key.
 	 */
 	StringInputData* get(int key);
+
+	/**
+	 * para imprimir el Hash por consola
+	 */
+	void print();
 };
 
 #endif /* HASH_H_ */
--- trunk/testHash/testHash.cpp
+++ trunk/testHash/testHash.cpp
@@ -6,53 +6,32 @@
 
 using namespace std;
 
-void test1() {
+void testTable() {
 	Table myTable;
 	myTable.createFile();
-	int sizeOfTable = myTable.getSize();
-	int listElementsTable[sizeOfTable];
 
-	myTable.parse(listElementsTable);
-	myTable.print(listElementsTable,sizeOfTable);
+	myTable.print();
 
 	myTable.modifyRegister(1,22);
-	sizeOfTable = myTable.getSize();
-	int listElementsTable2[sizeOfTable];
-	myTable.parse(listElementsTable2);
-	myTable.print(listElementsTable2,sizeOfTable);
+	myTable.print();
 
 	myTable.duplicate();
-	sizeOfTable = myTable.getSize();
-	int listElementsTable3[sizeOfTable];
-	myTable.parse(listElementsTable3);
-	myTable.print(listElementsTable3,sizeOfTable);
+	myTable.print();
 
 	myTable.duplicate();
-	sizeOfTable = myTable.getSize();
-	int listElementsTable4[sizeOfTable];
-	myTable.parse(listElementsTable4);
-	myTable.print(listElementsTable4,sizeOfTable);
+	myTable.print();
 
 	myTable.duplicate();
-	sizeOfTable = myTable.getSize();
-	int listElementsTable5[sizeOfTable];
-	myTable.parse(listElementsTable5);
-	myTable.print(listElementsTable5,sizeOfTable);
+	myTable.print();
 
 	/*myTable.modifyRegister(70,23);
-	sizeOfTable = myTable.getSize();
-	int listElementsTable6[sizeOfTable];
-	myTable.parse(listElementsTable6);
-	myTable.print(listElementsTable6,sizeOfTable);*/
+	myTable.print();*/
 
 	if (myTable.verifyAndDivide()!= 0)
 		printf("\nno se puede dividir");
 	else printf("\nSI se puede dividir");
 
-	sizeOfTable = myTable.getSize();
-	int listElementsTable7[sizeOfTable];
-	myTable.parse(listElementsTable7);
-	myTable.print(listElementsTable7,sizeOfTable);
+	myTable.print();
 }
 
 void test2() {
@@ -66,10 +45,11 @@ void test2() {
 	Hash* hash = new Hash();
 	hash->add(sid);
 
+
 }
 
 int main(){
-	//test1();
+	//testTable();
 	test2();
 	return 0;
 }
