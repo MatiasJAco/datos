162e59d7776853d1377c7d22b71b4e23e8308fd6
pablolisio <pablolisio@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-25 16:51:39 +0000

    elimino metodo que ya no sirve (se cambio la idea)

--- logic/hash/Hash.cpp
+++ logic/hash/Hash.cpp
@@ -21,46 +21,6 @@ Hash::~Hash() {
 	// TODO Auto-generated destructor stub
 }
 
-//void Hash::add(StringInputData* sid) {
-//	int hashTableSize = this->hashTable->getSize();
-//	int key = sid->getKey();
-//	int blockPositionInTable = key % hashTableSize;
-//	unsigned int bucketNumber = this->hashTable->getNumberOfBucketInHash(blockPositionInTable);
-//	cout << "Key: " << key << " Size: " << hashTableSize << " Position: " << blockPositionInTable << " Bucket number: " << bucketNumber << endl;
-//
-//	VarRegister* varRegister = new VarRegister();
-//	varRegister->setValue((char*)sid->getValue().c_str(), sid->getValue().size()+1);
-//
-//	/* Se intenta obtener el bloque desde el disco: */
-//	Block* block = this->hashFile->getBlock(bucketNumber);
-//
-//	/* Si no encuentra el bloque del archivo en donde debería ir la clave, crea el bucket junto a su respectivo bloque: */
-//	if (block == NULL) {
-//		this->buckets.push_back(Bucket(bucketNumber, hashTableSize));
-//
-//		Block* block = this->hashFile->getNewBlock();
-//		block->addRegister(*varRegister);
-//		this->hashFile->saveBlock(block);
-//	} else {
-//		/* Si el bloque posee suficiente espacio para guardar un registro más, lo guarda: */
-//		if (block->getRemainingSpace() >= varRegister->getDiskSize()) {
-//			block->addRegister(*varRegister);
-//			this->hashFile->saveBlock(block);
-//		} else { /* En caso contrario, se duplica la tabla y se guarda el registro en un bloque nuevo. */
-//			list<Bucket>::iterator bucket = this->buckets.begin();
-//			bool found = false;
-//			while((!found) && (bucket != this->buckets.end())) {
-//				if (bucket->getNumber() == bucketNumber) {
-//					bucket->setDepth(2); // TODO ver que valor vá acá.
-//				}
-//				found = true;
-//				bucket++;
-//			}
-//			//TODO completar qué hacer al dar de alta cuando se duplica la tabla.
-//		}
-//	}
-//}
-
 StringInputData* Hash::get(int key) {
 	int size = this->hashTable->getSize();
 	int blockPositionInTable = key % size;
@@ -70,9 +30,6 @@ StringInputData* Hash::get(int key) {
 	//file.getRegister(blockNumber);
 	return new StringInputData();
 }
-
-
-
 
 void Hash::inicializeHashFile(){
 	int depth=1;
