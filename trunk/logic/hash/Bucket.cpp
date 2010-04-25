c454aed7b50f1fbef8f0f1bb5394d6ce791a3c8b
pablolisio <pablolisio@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-25 15:19:10 +0000

    nuevos arreglos de hash

--- trunk/logic/hash/Bucket.cpp
+++ trunk/logic/hash/Bucket.cpp
@@ -7,10 +7,18 @@
 
 #include "Bucket.h"
 
-Bucket::Bucket(unsigned int number, unsigned int depth) {
-	this->number = number;
-	this->depth = depth;
+//Bucket::Bucket(unsigned int number, unsigned int depth) {
+//	this->number = number;
+//	this->depth = depth;
+//}
+Bucket::Bucket(Block* block) {
+	this->block=block;
+	this->number = block->getBlockNumber();
+	//this->depth = ( (VarRegister) block->getNextRegister(false) ).getValue();
+	//TODO: hacer un metodo que devuelva el td sacado desde el bloque
+	this->depth=1;
 }
+
 
 Bucket::~Bucket() {
 }
@@ -43,4 +51,44 @@ bool operator==(const Bucket &bucket1, const Bucket &bucket2) {
 }
 bool operator!=(const Bucket &bucket1, const Bucket &bucket2) {
 	return bucket1.number != bucket2.number;
+}
+
+
+void Bucket::setBlock(Block* block){
+	this->block = block;
+}
+
+Block* Bucket::getBlock(){
+	return this->block;
+}
+
+
+
+bool Bucket::add(StringInputData* sid){
+
+
+
+	VarRegister* varRegister = new VarRegister();
+
+
+	//hay que ver como hacemos para pasarle (clave+valor) en el primer parametro. ¿Como lo "desparseamos" desp?
+	//algo asi:
+	// char* valor = sid->getKey().c_str() + sid->getValue().c_str();
+	//varRegister->setValue(valor, sizeof(valor));      //usar el sizeof()
+	//TODO: este renglon de aca abajo esta mal, ver explicacion de arriba
+	varRegister->setValue((char*)sid->getValue().c_str(), sid->getValue().size()+1);
+
+
+	/* Si el bloque posee suficiente espacio para guardar un registro más, lo guarda: */
+	//TODO: cambiar este metodo por el nuevo q esta haciendo alex (mail que envio el 24/04/10)
+	if (this->block->getRemainingSpace() >= varRegister->getDiskSize()) {
+		this->block->addRegister(*varRegister);
+		//this->hashFile->saveBlock(this->block);
+	} else { /* En caso contrario, se duplica la tabla y se guarda el registro en un bloque nuevo. */
+
+
+		//TODO completar qué hacer al dar de alta cuando se duplica la tabla.
+	}
+
+	return true;
 }
--- trunk/logic/hash/Bucket.h
+++ trunk/logic/hash/Bucket.h
@@ -10,15 +10,18 @@
 
 #include "../../physical/file/Block.h"
 #include "../../physical/file/BlockFile.h"
+#include "../input/StringInputData.h"
 
 class Bucket {
 
 private:
 	unsigned int number; // Numero de cubo.
 	unsigned int depth; // Dispersion del cubo.
+	Block* block;
 
 public:
-	Bucket(unsigned int number, unsigned int depth);
+	//Bucket(unsigned int number, unsigned int depth);
+	Bucket(Block* block);
 	virtual ~Bucket();
 
 	void setNumber(unsigned int number);
@@ -30,6 +33,17 @@ public:
 	friend bool operator>(const Bucket &object1, const Bucket &object2);
 	friend bool operator==(const Bucket &object1, const Bucket &object2);
 	friend bool operator!=(const Bucket &object1, const Bucket &object2);
+
+	void setBlock(Block* block);
+	Block* getBlock();
+
+
+	/**
+	 * Agrega un elemento nuevo al bucket.
+	 * Si el StringInputData fue guardado exitosamente retorna true;
+	 * Si hubo desborde en el bucket este metodo retorna false;
+	 */
+	bool add(StringInputData* sid);
 };
 
 #endif /* BUCKET_H_ */
