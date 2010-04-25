9055b00a956544dfabed647f02b321ab2231c7c6
maldonado.celeste <maldonado.celeste@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-25 09:22:40 +0000

    Los metodos que faltaban en InputData para poder volcar los datos a un registro y viceversa

--- logic/input/InputData.h
+++ logic/input/InputData.h
@@ -11,6 +11,8 @@
 #include <iostream>
 #include <sstream>
 
+#include "../physical/utils/ByteConverter.h"
+
 using namespace std;
 
 /**
@@ -30,6 +32,27 @@ public:
 	virtual void setValue(std::stringstream& value) = 0;
 	virtual  int getKey()const = 0;
 	virtual string getValue() = 0;
+
+	/**
+	 * Devuelve el stream correspondiente al dato.
+	 * @param stream stream en donde se almacena.
+	 * @return El mismo stream.
+	 * El resultado de este metodo sera utilizado en las llamadas a setValue de Register.
+	 */
+	virtual char* toStream(char* stream) const = 0;
+
+	/**
+	 * Convierte el stream pasado por parametro a un InputData
+	 * @param stream Stream a convertir.
+	 */
+	virtual void toData(const char* stream) = 0;
+
+	/**
+	 * Tamaño del dato.
+	 */
+	virtual unsigned int size() = 0;
+
+
 };
 
 #endif /* INPUTDATA_H_ */
