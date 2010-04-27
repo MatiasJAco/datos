b98f3d18ce52175e2622d5939b72090c3e7f3814
pablolisio <pablolisio@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-27 03:05:29 +0000

    ya anda el metodo existsElement de hash

--- trunk/logic/hash/Table.cpp
+++ trunk/logic/hash/Table.cpp
@@ -182,7 +182,7 @@ void Table::print(){
 	int listElementsTable[sizeOfTable];
 	this->parse(listElementsTable);
 
-	printf("\nTabla (de tamaño = %i):\n ",sizeOfTable);
+	printf("Tabla (de tamaño = %i):\n ",sizeOfTable);
 	for (int i = 0; i<sizeOfTable;i++){
 		printf("%i",listElementsTable[i]);
 		if (i<sizeOfTable-1)
