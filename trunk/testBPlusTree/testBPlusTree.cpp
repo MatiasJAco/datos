9a8f85a849c6720a49c9311efb62c8f2c18ca509
matiasja <matiasja@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-04-29 10:24:54 +0000

    UT para BPlusTree en construccion.

---
+++ trunk/testBPlusTree/testBPlusTree.cpp
@@ -0,0 +1,18 @@
+/*
+ * testBPlusTree.cpp
+ *
+ *  Created on: 29/04/2010
+ *      Author: matias
+ */
+#include <iostream>
+#include <sstream>
+#include "../logic/tree/BPlusTree.h"
+
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	BPlusTree miArbol = new BPlusTree(3,50);
+
+}
