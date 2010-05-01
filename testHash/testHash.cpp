5bc15bec357721b16134cb050ab6b7e1f994d676
coghlaniano <coghlaniano@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-05-01 18:36:29 +0000

    Separo la prueba de borrar de la de modificar.

--- testHash/testHash.cpp
+++ testHash/testHash.cpp
@@ -68,6 +68,28 @@ StringInputData* createSid( int key,char * value){
 	return sid;
 }
 
+void testModify() {
+	Hash* hash = new Hash();
+	/* Se toma el dato que ingresa el usuario. */
+	char value[8] = "paytiti";
+	for (int i = 1; i<=70;i++){
+		StringInputData* sid = createSid(i,value);
+		hash->add(sid);
+		delete sid;
+	}
+
+	hash->print();
+	char newValue[15]= "______________";
+	bool result = hash->modify(12,newValue);
+	if (result==1)
+		cout<<endl<<"NO existe el sid"<<endl;
+	else if (result==-1)
+		cout<<endl<<"Hubo algun prob"<<endl;
+	else
+		cout<<endl<<"lo cambio!"<<endl;
+	hash->print();
+}
+
 void testDelete() {
 	Hash* hash = new Hash();
 	char value[8] = "paytiti";
@@ -85,7 +107,6 @@ void testDelete() {
 
 	hash->print();
 
-//	hash->erase(11);
 	hash->erase(22);
 
 	hash->print();
@@ -96,29 +117,11 @@ void tests() {
 	Hash* hash = new Hash();
 	/* Se toma el dato que ingresa el usuario. */
 	char value[8] = "paytiti";
-	for (int i = 1; i<=61;i++){
+	for (int i = 1; i<=70;i++){
 		StringInputData* sid = createSid(i,value);
 		hash->add(sid);
 		delete sid;
 	}
-//	hash->print();
-	for (int i = 62; i<=70;i++){
-		StringInputData* sid = createSid(i,value);
-		hash->add(sid);
-		delete sid;
-	}
-
-//	hash->print();
-	//@return int Retorna 1 si el sid no existe;0 si fue cambiado; -1 si hubo algun problema
-//	char newValue[15]= "______________";
-//	bool result = hash->modify(12,newValue);
-//	if (result==1)
-//		cout<<endl<<"NO existe el sid"<<endl;
-//	else if (result==-1)
-//		cout<<endl<<"Hubo algun prob"<<endl;
-//	else
-//			cout<<endl<<"lo cambio!"<<endl;
-
 
 	hash->erase(70);
 	hash->erase(66);
@@ -150,77 +153,77 @@ int main(int argc, const char* argv[]){
 	//testTable3();
 	//testTable4();
 	//testDelete();
-	//tests();
+	tests();
 	//cout << "paso test!" << endl;
 
-	Hash* hash = new Hash();
-
-	if (argc == 1 || argc == 2) {
-		cout << "Comandos para usar el hash:" << endl;
-		cout << "-B (Busca una clave)" << endl;
-		cout << "-I (Ingresa un dato a la estructura" << endl;
-		cout << "-M (Modifica un dato existente de la estructura)" << endl;
-		cout << "-Q (Quita un dato de la estructura)" << endl;
-		cout << "-S (Impresion de datos actuales de la estructura)" << endl;
-		cout << "-h (Ayuda)" << endl;
-		return 0;
-	}
-
-	string operacion (argv[1]);
-	string claveString (argv[2]);
-	string valor (argv[3]);
-	//string aux (";");
-
-	//int separator = claveValor.find(aux);
-
-	//int clave = atoi(claveValor.substr(0,separator).c_str());
-	//string valor = claveValor.substr(separator+1,claveValor.size());
-
-	int clave = atoi(claveString.c_str());
-	if (operacion == "-B") {
-		cout << "Buscando la clave " << clave << "..." << endl;
-		StringInputData* sid = hash->get(clave);
-		if (sid != NULL) {
-			cout << sid->toString() << endl;
-		} else {
-			cout << "No se encontro la clave " << clave << endl;
-		}
-	} else if (operacion == "-I") {
-		cout << "Ingresando la clave" << clave << " con el valor " << valor << "..." << endl;
-		int addResult = hash->add(clave, valor);
-		if (addResult == 0) {
-			cout << "Se agrego correctamente la clave " << clave << endl;
-		} else if (addResult == 1) {
-			cout << "No se agrego la clave " << clave << " pues ya existia."<< endl;
-		} else {
-			cout << "Se produjo un error al ingresar la clave " << clave << endl;
-		}
-	} else if (operacion == "-M") {
-		cout << "Modificando el valor de la clave" << clave << " por " << valor << "..." << endl;
-		int modifyResult = hash->modify(clave, valor);
-		if (modifyResult == 0) {
-			cout << "Se modifico correctamente el valor de la clave " << clave << endl;
-		} else if (modifyResult == 1) {
-			cout << "No se modifico el valor de la clave " << clave << " pues no existia la misma."<< endl;
-		} else {
-			cout << "Se produjo un error al modificar el valor de la clave " << clave << endl;
-		}
-	} else if (operacion == "-Q") {
-		cout << "Quitando el dato representado por la clave " << clave << "..." << endl;
-		int eraseResult = hash->erase(clave);
-		if (eraseResult == 0) {
-			cout << "Se elimino correctamente la clave " << clave << endl;
-		} else if (eraseResult == 1) {
-			cout << "No se elimino la clave " << clave << " pues no existia la misma."<< endl;
-		} else {
-			cout << "Se produjo un error al eliminar la clave " << clave << endl;
-		}
-	} else if (operacion == "-S") {
-		cout << "Impresion del estado actual:" << endl;
-		hash->print();
-	} else if (operacion == "-h") {
-		cout << "Ayuda" << endl;
-	}
-	delete hash;
+//	Hash* hash = new Hash();
+//
+//	if (argc == 1 || argc == 2) {
+//		cout << "Comandos para usar el hash:" << endl;
+//		cout << "-B (Busca una clave)" << endl;
+//		cout << "-I (Ingresa un dato a la estructura" << endl;
+//		cout << "-M (Modifica un dato existente de la estructura)" << endl;
+//		cout << "-Q (Quita un dato de la estructura)" << endl;
+//		cout << "-S (Impresion de datos actuales de la estructura)" << endl;
+//		cout << "-h (Ayuda)" << endl;
+//		return 0;
+//	}
+//
+//	string operacion (argv[1]);
+//	string claveString (argv[2]);
+//	string valor (argv[3]);
+//	//string aux (";");
+//
+//	//int separator = claveValor.find(aux);
+//
+//	//int clave = atoi(claveValor.substr(0,separator).c_str());
+//	//string valor = claveValor.substr(separator+1,claveValor.size());
+//
+//	int clave = atoi(claveString.c_str());
+//	if (operacion == "-B") {
+//		cout << "Buscando la clave " << clave << "..." << endl;
+//		StringInputData* sid = hash->get(clave);
+//		if (sid != NULL) {
+//			cout << sid->toString() << endl;
+//		} else {
+//			cout << "No se encontro la clave " << clave << endl;
+//		}
+//	} else if (operacion == "-I") {
+//		cout << "Ingresando la clave" << clave << " con el valor " << valor << "..." << endl;
+//		int addResult = hash->add(clave, valor);
+//		if (addResult == 0) {
+//			cout << "Se agrego correctamente la clave " << clave << endl;
+//		} else if (addResult == 1) {
+//			cout << "No se agrego la clave " << clave << " pues ya existia."<< endl;
+//		} else {
+//			cout << "Se produjo un error al ingresar la clave " << clave << endl;
+//		}
+//	} else if (operacion == "-M") {
+//		cout << "Modificando el valor de la clave" << clave << " por " << valor << "..." << endl;
+//		int modifyResult = hash->modify(clave, valor);
+//		if (modifyResult == 0) {
+//			cout << "Se modifico correctamente el valor de la clave " << clave << endl;
+//		} else if (modifyResult == 1) {
+//			cout << "No se modifico el valor de la clave " << clave << " pues no existia la misma."<< endl;
+//		} else {
+//			cout << "Se produjo un error al modificar el valor de la clave " << clave << endl;
+//		}
+//	} else if (operacion == "-Q") {
+//		cout << "Quitando el dato representado por la clave " << clave << "..." << endl;
+//		int eraseResult = hash->erase(clave);
+//		if (eraseResult == 0) {
+//			cout << "Se elimino correctamente la clave " << clave << endl;
+//		} else if (eraseResult == 1) {
+//			cout << "No se elimino la clave " << clave << " pues no existia la misma."<< endl;
+//		} else {
+//			cout << "Se produjo un error al eliminar la clave " << clave << endl;
+//		}
+//	} else if (operacion == "-S") {
+//		cout << "Impresion del estado actual:" << endl;
+//		hash->print();
+//	} else if (operacion == "-h") {
+//		cout << "Ayuda" << endl;
+//	}
+//	delete hash;
 	return 0;
 }
