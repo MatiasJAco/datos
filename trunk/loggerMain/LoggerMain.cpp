b5f09d6eedbd9dad8438d8e80fe151ecd2a06924
coghlaniano <coghlaniano@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-05-02 16:48:33 +0000

    Se agrega un main para correr el logger desde la consola (para la entrega).

---
+++ trunk/loggerMain/LoggerMain.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <sstream>
+#include "../logic/input/StringInputData.h"
+#include "../logic/logger/Logger.h"
+
+using namespace std;
+
+int main(int argc, const char* argv[]){
+	Logger* logger = new Logger();
+
+	if (argc == 1) {
+		cout << "Comandos para usar el logger:" << endl;
+		cout << "-B (Busca una cadena de caracteres)" << endl;
+		cout << "-I (Ingresa una cadena de caracteres a la estructura" << endl;
+		cout << "-S (Impresion de datos actuales de la estructura)" << endl;
+		cout << "-h (Ayuda)" << endl;
+		delete logger;
+		return 0;
+	} else if (argc == 2) { // Solo se permiten las operaciones S, y H.
+		string operacion (argv[1]);
+		if (operacion == "-S") {
+			cout << "Impresion del estado actual:" << endl;
+			logger->recorrer_log();
+			delete logger;
+			return 0;
+		} else if (operacion == "-h") {
+			cout << "Ayuda - Algunos ejemplos de ejecucion:" << endl;
+			cout << "./loggerMain -I \"(12;paytiti)\"" << endl;
+			cout << "./loggerMain -B \"(12;)\"" << endl;
+			cout << "./loggerMain -S" << endl;
+			delete logger;
+			return 0;
+		} else {
+			cout << "Faltan parametros. Utilice \"./loggerMain -h\" para obtener mas ayuda. " << endl;
+			delete logger;
+			return 0;
+		}
+	}
+
+	string operacion (argv[1]);
+	string cadena (argv[2]);
+
+	if (operacion == "-B") {
+		cout << "Buscando la cadena de caracteres " << cadena << "..." << endl;
+		logger->buscar_cadena(&cadena[0]); //TODO hacer saber si se encontro o no.
+	} else if (operacion == "-I") {
+		cout << "Ingresando la cadena de caracteres " << cadena << endl;
+		logger->ingresar(&cadena[0]);
+		cout << "Se ingreso correctamente la cadena de caracteres " << cadena << endl;
+	} else if (operacion == "-S") {
+		cout << "Impresion del estado actual:" << endl;
+		logger->recorrer_log();
+	} else if (operacion == "-h") {
+		cout << "Ayuda - Algunos ejemplos de ejecucion:" << endl;
+		cout << "./loggerMain -I \"(12;paytiti)\"" << endl;
+		cout << "./loggerMain -B \"(12;)\"" << endl;
+		cout << "./loggerMain -M \"(12;pepe)\"" << endl;
+		cout << "./loggerMain -Q \"(12;)\"" << endl;
+		cout << "./loggerMain -S" << endl;
+	}
+	delete logger;
+	return 0;
+}
