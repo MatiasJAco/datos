827ce9c7bab70a31eb974258b99bbacc41c0107d
pablolisio <pablolisio@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-06-07 20:41:24 +0000

    inicio de estructura de clase de ppmc

---
+++ trunk/logic/ppmc/Ppmc.cpp
@@ -0,0 +1,24 @@
+/*
+ * Ppmc.cpp
+ *
+ *  Created on: Jun 7, 2010
+ *      Author: Adrián Bernardi - Matias Acosta - Pablo Lisio
+ */
+#include "Ppmc.h"
+
+Ppmc::Ppmc(){
+
+}
+
+Ppmc::~Ppmc() {
+
+}
+
+bool Ppmc::compress(std::string path,int context){
+	std::cout<<path;
+	return true;
+}
+
+bool Ppmc::deCompress(const std::string & path){
+	return false;
+}
