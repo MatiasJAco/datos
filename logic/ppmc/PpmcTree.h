1c6cfab3e6381c0bca5e3617aba31c60fbf818ea
pablolisio <pablolisio@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-06-08 20:57:02 +0000

    Defini un par de metodos comunes para ppmchash y ppmctree  y otros que no van a ser comunes, hay que revisarlo esto, esta hecho en el aire.

--- logic/ppmc/PpmcTree.h
+++ logic/ppmc/PpmcTree.h
@@ -11,9 +11,32 @@
 #include "Ppmc.h"
 
 class PpmcTree: public Ppmc {
+
+private:
+
+	/**
+	 * Guarda en el Arbol la clave context y el valor value
+	 * @param context Es la clave a guardar en la estructura
+	 * @param value Es el valor a guardar en la estructura
+	 * @return Devuelve true si lo guardo correctamente. En caso contrario devuelve false.
+	 */
+	bool saveContext(const char* context, std::string value);
+
+	/**
+	 * Busca en el Arbol la clave context y devuelve el valor value
+	 * @param context Es la clave a buscar en la estructura
+	 * @param character Es el caracter "dado character"
+	 * @param value Es el valor de la clave (la probabilidad). Este valor lo devuelve
+	 * @return Devuelve true si lo encontro. En caso contrario devuelve false.
+	 */
+	bool findContext(const char* context, char character, std::string & value);
+
+
 public:
+
 	PpmcTree();
 	virtual ~PpmcTree();
+
 };
 
 #endif /* PPMCTREE_H_ */
