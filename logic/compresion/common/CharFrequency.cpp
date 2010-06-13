94fc78a696cd92fe4b7ae59a0478f54589f5c086
maldonado.celeste <maldonado.celeste@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-06-13 20:38:11 +0000

    Se agregaron clases para manejo de la tabla de frecuencias. Van a estar en comun entre el PPMC y el compresor aritmetico. La idea es que la tabla sea el valor que se guarda en stringInputData. Solo se define la api, para que celes pueda seguir con el compresor aritmetico la parte de ppmc se deberia pasar a la carpeta compresion

---
+++ logic/compresion/common/CharFrequency.cpp
@@ -0,0 +1,17 @@
+/*
+ * CharFrequency.cpp
+ *
+ *  Created on: 13/06/2010
+ *      Author: celeste
+ */
+
+#include "CharFrequency.h"
+
+CharFrequency::CharFrequency() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CharFrequency::~CharFrequency() {
+	// TODO Auto-generated destructor stub
+}
