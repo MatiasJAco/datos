da27b063c6a09a3dd67bd838c120ee8c985758f0
maldonado.celeste <maldonado.celeste@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-06-15 02:17:41 +0000

    Subo implementación de BitFile, para poder escribir en el archivo comprimido, bits unos o ceros...
    Les cuento que con alex estuvimos hablando y quedamos en que el 
    compresor aritmetico va a tener el archivo a comprimir y/o descomprimir.
    
    De esa forma el compresor aritmetico puede existir incluso sin
    un ppmc...
    
    El ppmc entonces tendria una instancia del compresor aritmetico, pero 
    el que se encarga de emitir sobre el archivo los caracteres comprimidos
    va a ser el aritmetico...
    
    asi que de esa parte no tendrian que preocuparse.
    
    Igual falta todavia definir totalmente la Api del aritmetico...
    
    El miercoles cualquier cosa lo vemos bien.

---
+++ trunk/physical/file/BitFile.cpp
@@ -0,0 +1,114 @@
+/*
+ * BitFile.cpp
+ *
+ *  Created on: 14/06/2010
+ *      Author: kira
+ */
+
+#include "BitFile.h"
+
+BitFile::BitFile(accessModeEnum type) {
+	// TODO Auto-generated constructor stub
+	m_byteFile = new SequentialFile(type);
+
+	if (type == WRITE_FILE)
+		clearBuffer();
+	else
+		readBuffer();
+}
+
+BitFile::~BitFile() {
+	// TODO Auto-generated destructor stub
+	delete m_byteFile;
+}
+
+
+bool BitFile::open(const std::string fileName)
+{
+	return m_byteFile->open(fileName);
+}
+
+
+bool BitFile::close()
+{
+	return m_byteFile->close();
+}
+
+void BitFile::write(Bit bit) throw (PhysicalException)
+{
+	if (bit&ONE)
+		writeBitOne();
+	else
+		writeBitZero();
+}
+
+Bit BitFile::read()
+{
+	Bit bit = ZERO;
+
+	if (m_posBit == 0)
+	{
+		if (m_buffer&1)
+			bit = ONE;
+		readBuffer();
+	}
+	else
+	{
+		if (m_buffer&(1 << m_posBit))
+			bit = ONE;
+		m_posBit--;
+	}
+
+	return bit;
+}
+
+
+void BitFile::readBuffer() throw (PhysicalException)
+{
+	m_buffer = m_byteFile->readChar();
+	m_posBit = SIZE_BUFFER-1;
+}
+
+bool BitFile::clearBuffer()
+{
+	bool bret = true;
+
+	m_buffer = 0;
+	m_posBit = SIZE_BUFFER-1;
+
+	return bret;
+}
+
+
+void BitFile::writeBitOne() throw (PhysicalException)
+{
+	if (m_posBit == 0)
+	{
+		m_byteFile->writeChar((char)(m_buffer+1));
+		clearBuffer();
+	}
+	else
+	{
+		m_buffer = (m_buffer+1)<< 1;
+		m_posBit--;
+	}
+}
+
+
+
+void BitFile::writeBitZero() throw (PhysicalException)
+{
+	if (m_posBit == 0)
+	{
+		m_byteFile->writeChar((char)m_buffer);
+		clearBuffer();
+	}
+	else
+	{
+		m_buffer = m_buffer << 1;
+		m_posBit--;
+	}
+}
+
+
+
