e7799f49a5379f756ce28d85432b35481c93c0c7
maldonado.celeste <maldonado.celeste@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-06-17 19:59:10 +0000

    Subo arithmeticCompressor
    Dos cosas importantes: 
    Si descomprimen, crean una instancia,
    si comprimen, tienen que crear otra instancia.
    
    El archivo que le pasan al constructor:
    es el archivo resultado de la compresion si comprimen
    es el archivo comprimido si van a descomprimir..
    
    Miren los comentarios de como manejar compress, decompress
    
    
    Decompress devuelve un short que corresponde al caracter que pudo
    descomprimir. 
    En base a eso ustedes tendran que actualizar la
    tabla de frecuencias, etc...ustedes veran como manejar eso con el ppmc.
    
    
    Bueno, yo me voy a Lacuna, no deberia ir, pero no se cuando volveran otra vez...:P

---
+++ trunk/application/compresion/arithmetic/ArithmeticCompressor.cpp
@@ -0,0 +1,50 @@
+/*
+ * ArithmeticCompressor.cpp
+ *
+ *  Created on: 16/06/2010
+ *      Author: kira
+ */
+
+#include "ArithmeticCompressor.h"
+
+ArithmeticCompressor::ArithmeticCompressor(Coder coder,const std::string fileName,unsigned int maxbits)
+{
+	// TODO Auto-generated constructor stub
+	m_coder = coder;
+	m_maxbits = maxbits;
+
+	m_floor = 0;
+	m_roof = pow(2,maxbits)-1;
+
+	if (m_coder == COMPRESSOR)
+		m_bitFile = new BitFile(WRITE_FILE);
+	else
+		m_bitFile = new BitFile(READ_FILE);
+
+	m_bitFile->open(fileName);
+}
+
+ArithmeticCompressor::~ArithmeticCompressor() {
+	// TODO Auto-generated destructor stub
+	m_bitFile->close();
+	delete m_bitFile;
+}
+
+void ArithmeticCompressor::compress(short symbol,FrequencyTable & ft)
+{
+	m_floor = getFloorSymbol(symbol,ft);
+	m_roof = getRoofSymbol(symbol,ft);
+
+	normalize();
+
+	if (overflow())
+		emit();
+}
+
+short ArithmeticCompressor::decompress(FrequencyTable& ft)
+{
+//	getSymbol(num,ft);
+
+	return 0;
+}
+
