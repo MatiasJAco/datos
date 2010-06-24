ac5197e4b1df49e611d012bc004562bd58570a98
alexmantaut <alexmantaut@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-06-24 19:31:07 +0000

    Se sube la ultima version del testeo de la tabla de frec para revisar si hay error con el esc

--- testFrequencyTable/testFrequencyTable.cpp
+++ testFrequencyTable/testFrequencyTable.cpp
@@ -25,19 +25,27 @@ void testFreqTable()
 	ft->setFrequency('b',5);
 	ft->setFrequency('/',5);
 	ft->setFrequency('\\',5);
+	ft->setFrequency(EOF_CHAR,5);
+	ft->setFrequency(ESC_CHAR,5);
 
 	ft->increaseFrequency('c',4);
-
+	ft->increaseFrequency(ESC_CHAR,2);
 	serial = ft->toString();
 
 	cout << serial<<endl;
 
 	ft2->deserialize(serial);
+	ft2->increaseFrequency(ESC_CHAR,4);
 
 	cout << ft2->toString()<<endl;
 
-	cout<< ft->getFrequency('c')<<endl;
+
+	cout<<"cum frec "<< ft->getCumFrequency('c')<<endl;
+	cout<<"cum frec2 "<< ft->getCumFrequency('j')<<endl;
+	cout<< "frec "<<ft->getFrequency('c')<<endl;
+	cout<< "frec2 "<<ft->getFrequency('j')<<endl;
 	cout <<"char"<< (char) 98<<endl;
+	cout <<"esc char"<<ft2->getFrequency(ESC_CHAR)<<endl;
 	cout <<"total1"<< ft->getFrequencyTotal()<<endl;
 	cout <<"total2"<< ft2->getFrequencyTotal()<<endl;
 
