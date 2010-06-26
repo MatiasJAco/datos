4e76e24ac7480cde66e75da0dd19ab73c65d6c27
coghlaniano <coghlaniano@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-06-26 02:19:05 +0000

    Subo principio de código de exclusión, para la compresión del PPMC.

--- trunk/testHash/testHash.cpp
+++ trunk/testHash/testHash.cpp
@@ -292,46 +292,72 @@ void testHashFunction() {
 	std::cout << (f % g).toInt() << std::endl;
 }
 
+
 void testFreqTable()
 {
         FrequencyTable *ft;
         FrequencyTable *ft2;
+        FrequencyTable *ft3;
+        FrequencyTable ft4;
         string serial, deserial;
 
         ft = new FrequencyTable();
         ft2 = new FrequencyTable();
+        ft3 = new FrequencyTable();
 
         ft->setFrequency('a',2);
         ft->setFrequency('d',3);
         ft->setFrequency('c',4);
         ft->setFrequency('b',5);
         ft->setFrequency('/',5);
-        ft->setFrequency('\\',5);
+        ft->setFrequency('\\',7);
+        ft->setFrequency(258,1000);
+        ft->setFrequency(EOF_CHAR,5);
+        ft->setFrequency(ESC_CHAR,5);
 
         ft->increaseFrequency('c',4);
+        ft->increaseFrequency(ESC_CHAR,2);
+
+        ft3->setFrequency('b',2);
+        ft3->setFrequency('\\',2);
+        ft3->setFrequency('j',2);
+        ft3->setFrequency(ESC_CHAR,5);
 
         serial = ft->toString();
 
         cout << serial<<endl;
 
         ft2->deserialize(serial);
+        ft2->increaseFrequency(ESC_CHAR,4);
 
         cout << ft2->toString()<<endl;
 
-        cout<< ft->getFrequency('c')<<endl;
+
+        cout<<"cum frec "<< ft->getCumFrequency('c')<<endl;
+        cout<<"cum frec2 "<< ft->getCumFrequency('j')<<endl;
+        cout<< "frec "<<ft->getFrequency('c')<<endl;
+        cout<< "frec2 "<<ft->getFrequency('j')<<endl;
         cout <<"char"<< (char) 98<<endl;
+        cout <<"esc char"<<ft2->getFrequency(ESC_CHAR)<<endl;
         cout <<"total1"<< ft->getFrequencyTotal()<<endl;
         cout <<"total2"<< ft2->getFrequencyTotal()<<endl;
 
+        //ft->excludeFromTable(*ft2);
+
+        ft4 = ft->excludeFromTable(*ft3);
+
+        cout << "ft4 " << ft4.toString()<<endl;
+
+
         delete ft;
         delete ft2;
-
+        delete ft3;
 }
 
 void testPpmcHashComprimir() {
 	GeneralStructure* hash = new Hash();
 	Ppmc* ppmcHash = new PpmcHash(hash);
-	ppmcHash->compress("./archivoAComprimir.txt", 3);
+	ppmcHash->compress("./archivoAComprimir.txt", 2);
 	hash->deleteGeneratedFiles();
 }
 void testPpmcHashDescomprimir() {
@@ -358,8 +384,8 @@ int main(int argc, const char* argv[]){
 	//testFreqTable();
 
 	/* TESTS DE PPMC */
-	//testPpmcHashComprimir();
-	testPpmcHashDescomprimir();
+	testPpmcHashComprimir();
+	//testPpmcHashDescomprimir();
 
 	/* TESTS PARA LA FUNCION HASH NUEVA */
 	//testBigInt();
