97d386b424be9d5c510a0f00535b27ebe89b305a
coghlaniano <coghlaniano@89cd4edc-2be9-11df-8e64-fd3be38ee323>
2010-06-27 15:41:18 +0000

    Subo más pruebas al proyecto "Tests". ahora si están todas.

---
+++ trunk/Tests/TestFrequencyTable/TestFrequencyTable.cpp
@@ -0,0 +1,77 @@
+/*
+ * TestFrequencyTable.cpp
+ *
+ *  Created on: Jun 27, 2010
+ *      Author: Alex
+ */
+
+#include "TestFrequencyTable.h"
+#include "../application/compresion/common/FrequencyTable.h"
+#include "../application/compresion/common/CharFrequency.h"
+#include <iostream>
+
+TestFrequencyTable::TestFrequencyTable() {
+}
+
+TestFrequencyTable::~TestFrequencyTable() {
+}
+
+void TestFrequencyTable::testFreqTable() {
+	FrequencyTable *ft;
+	FrequencyTable *ft2;
+	FrequencyTable *ft3;
+	FrequencyTable ft4;
+	std::string serial, deserial;
+
+	ft = new FrequencyTable();
+	ft2 = new FrequencyTable();
+	ft3 = new FrequencyTable();
+
+	ft->setFrequency('a',2);
+	ft->setFrequency('d',3);
+	ft->setFrequency('c',4);
+	ft->setFrequency('b',5);
+	ft->setFrequency('/',5);
+	ft->setFrequency('\\',7);
+	ft->setFrequency(258,1000);
+	ft->setFrequency(EOF_CHAR,5);
+	ft->setFrequency(ESC_CHAR,5);
+
+	ft->increaseFrequency('c',4);
+	ft->increaseFrequency(ESC_CHAR,2);
+
+	ft3->setFrequency('b',2);
+	ft3->setFrequency('\\',2);
+	ft3->setFrequency('j',2);
+	ft3->setFrequency(ESC_CHAR,5);
+
+	serial = ft->toString();
+
+	std::cout << serial<<std::endl;
+
+	ft2->deserialize(serial);
+	ft2->increaseFrequency(ESC_CHAR,4);
+
+	std::cout << ft2->toString()<<std::endl;
+
+
+	std::cout<<"cum frec "<< ft->getCumFrequency('c')<<std::endl;
+	std::cout<<"cum frec2 "<< ft->getCumFrequency('j')<<std::endl;
+	std::cout<< "frec "<<ft->getFrequency('c')<<std::endl;
+	std::cout<< "frec2 "<<ft->getFrequency('j')<<std::endl;
+	std::cout <<"char"<< (char) 98<<std::endl;
+	std::cout <<"esc char"<<ft2->getFrequency(ESC_CHAR)<<std::endl;
+	std::cout <<"total1"<< ft->getFrequencyTotal()<<std::endl;
+	std::cout <<"total2"<< ft2->getFrequencyTotal()<<std::endl;
+
+	//ft->excludeFromTable(*ft2);
+
+	ft4 = ft->excludeFromTable(*ft3);
+
+	std::cout << "ft4 " << ft4.toString()<<std::endl;
+
+
+	delete ft;
+	delete ft2;
+	delete ft3;
+}
---
+++ trunk/Tests/TestStadistic/TestStadistic.cpp
@@ -0,0 +1,26 @@
+/*
+ * TestStadistic.cpp
+ *
+ *  Created on: Jun 27, 2010
+ *      Author: adrian
+ */
+
+#include "TestStadistic.h"
+#include "../logic/ppmc/stadistic/stadistic.h"
+
+TestStadistic::TestStadistic() {
+}
+
+TestStadistic::~TestStadistic() {
+}
+
+void TestStadistic::testStadistic() {
+	char  path [20];
+
+	Stadistic* miEstadista=new Stadistic();
+	strcpy(path,"archivo");
+	int tamanio=miEstadista->getFileSize(path);
+
+	printf("El archivo es de %d",tamanio);
+	delete miEstadista;
+}
